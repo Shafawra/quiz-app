7caceb4e68db444ecf880a72fc7772365b052922
Shafawra <shafaaura35@gmail.com>
2025-10-03 18:59:53 +0800

    menambahkan beberapa fungsi untuk menjalankan quiz

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const questions = [
         answers: [
             {text: "15", correct: true},
             {text: "5", correct: false},
-            {text: "10", correct: true},
+            {text: "10", correct: false},
             {text: "2", correct: false},
         ]
     },
@@ -23,8 +23,8 @@ const questions = [
         answers: [
             {text: "2 tahun yang lalu", correct: false},
             {text: "Kemarin", correct: false},
-            {text: "Masa lalu", correct: true},
-            {text: "Hari esok", correct: false},
+            {text: "Masa lalu", correct: false},
+            {text: "Hari esok", correct: true},
         ]
     },
     {
@@ -32,22 +32,22 @@ const questions = [
         answers: [
             {text: "10", correct: false},
             {text: "1", correct: true},
-            {text: "0", correct: true},
+            {text: "0", correct: false},
             {text: "2", correct: false},
         ]
     }
 ]
 
 const questionElement = document.getElementById("question");
-const answerBtn = document.querySelector("answerBtn");
+const answerBtn = document.getElementById("answerBtn");
 const nextBtn = document.getElementById("nextBtn");
 
 
-let currontQuestionIndex = 0;
+let currentQuestionIndex = 0;
 let score = 0;
 
 function startQuiz() {
-    currontQuestionIndex = 0;
+    currentQuestionIndex = 0;
     score = 0;
     nextBtn.innerHTML = "Next";
     showQuestion();
@@ -55,11 +55,10 @@ function startQuiz() {
 
 function showQuestion() {
     resetState();
-}
 
 // tampilkan pertanyaan
-let currontQuestion = questions[currontQuestionIndex];
-let questionNo = currontQuestionIndex + 1;
+let currontQuestion = questions[currentQuestionIndex];
+let questionNo = currentQuestionIndex + 1;
 questionElement.innerHTML = questionNo + ". " + currontQuestion.question;
 
 // bikin button untuk tiap jawaban 
@@ -75,4 +74,60 @@ currontQuestion.answers.forEach(answer => {
 
     button.addEventListener("click", selectAnswer);
 });
+}
+
+function resetState() {
+    nextBtn.style.display = "none";
+    while(answerBtn.firstChild){
+        answerBtn.removeChild(answerBtn.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+
+    if(isCorrect){
+        selectedBtn.classList.add("bg-green-600");
+        score++
+    } else {
+        selectedBtn.classList.add("bg-red-600")
+    }
+
+    // bikin semua tombol disable setelah dipilih
+    Array.from(answerBtn.children).forEach(button => {
+        if (button.dataset.correct === "true"){
+            button.classList.add("bg-green-600");
+        }
+        button.disabled = true;
+    });
+
+    nextBtn.style.display = "block"
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `Quiz selesai! 🎉<br>Skor kamu: ${score} dari ${questions.length}`;
+    nextBtn.innerHTML = "Play Again";
+    nextBtn.style.display = "block";
+}
+
+function handleNextBtn(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextBtn.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextBtn();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
 
